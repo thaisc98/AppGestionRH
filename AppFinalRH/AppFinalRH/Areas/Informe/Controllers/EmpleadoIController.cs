284b4bb91bb416e7360b42c2b26ea16f2c7f8a93
Tнαιѕ Cσηтяєяαѕ <thaisc98@hotmail.es>
2019-03-30 17:39:29 -0500

    Corrección EmpleadoI

--- AppFinalRH/AppFinalRH/Areas/Informe/Controllers/EmpleadoIController.cs
+++ AppFinalRH/AppFinalRH/Areas/Informe/Controllers/EmpleadoIController.cs
@@ -13,27 +13,32 @@ namespace AppFinalRH.Areas.Informe.Controllers
             empldn = new EmpleadoLDN();
         }
         // GET: Informe/EmpleadoI
-        public ActionResult Index(string estado, string buscarPor, string buscar)
+        public ActionResult Index(string Estatus, string buscarPor, string buscar)
         {
-
             if (buscarPor == "Departamento")
             {
-                return View(empldn.GetActives().Where(x => x.Departamento.CodigoDep.StartsWith(buscar) || buscar == null));
+                return View(empldn.GetAll().Where(x => x.Departamento.CodigoDep.StartsWith(buscar) || buscar == null));
             }
             else if (buscarPor == "Nombre")
             {
-                return View(empldn.GetActives().Where(x => x.Nombre.StartsWith(buscar) || buscar == null));
+                return View(empldn.GetAll().Where(x => x.Nombre.StartsWith(buscar) || buscar == null));
             }
 
-            ViewBag.Status = (estado == null ? "A" : estado);
-            if (estado == null)
+
+            ViewBag.Estatus = (Estatus == null ? "A" : Estatus);
+            if (Estatus == "A")
             {
                 return View(empldn.GetActives());
             }
-            else
+            else if (Estatus == "I")
             {
                 return View(empldn.GetInactives());
             }
+
+
+           
+           
+            return View(empldn.GetActives());
 
         }
     }
