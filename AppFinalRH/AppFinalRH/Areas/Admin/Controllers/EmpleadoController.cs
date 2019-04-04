c65f3b7c87e481f9849b81e903e90db8b9ed0323
Miguel319 <miguelp90.319@gmail.com>
2019-04-04 00:49:29 -0400

    Filtro de empleados y mejora de paginación

--- AppFinalRH/AppFinalRH/Areas/Admin/Controllers/EmpleadoController.cs
+++ AppFinalRH/AppFinalRH/Areas/Admin/Controllers/EmpleadoController.cs
@@ -27,29 +27,149 @@ namespace AppFinalRH.Areas.Admin.Controllers
             ViewBag.SortOrder = SortOrder;
             ViewBag.SortBy = SortBy;
 
+            var empleados = empleadEmpleadoLDN.GetActives();
 
-            var empleados = empleadEmpleadoLDN.GetAll();
+            ViewBag.SortOrder = SortOrder;
+            ViewBag.SortBy = SortBy;
 
-            /*
-            switch (SortBy)
+
+            switch (SortOrder)
             {
-                switch ("Nombre")
-                {
-
-                    case "Asc":
-                        empleados = empleados.OrderBy(x => x.Nombre).ToList();
-                        break;
-                    case "Desc":
-                        empleados = empleados.OrderByDescending(x => x.Nombre).ToList();
-                        break;
-                }
+                case "CodigoEmp":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.CodigoEmp).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.CodigoEmp).ToList();
+                            break;
+                        default:
+                            break;
+                    }
                     break;
-                case
-        }*/
-            
+
+                case "Nombre":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.Nombre).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.Nombre).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "Apellido":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.Apellido).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.Apellido).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+
+                case "Telefono":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.Telefono).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.Telefono).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Departamento":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.Departamento.NombreDep).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.Departamento.NombreDep).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Cargo":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.Cargo.NombreCargo).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.Cargo.NombreCargo).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "FechaIngreso":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.FechaIngreso).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.FechaIngreso).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Salario":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.Salario).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.Salario).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case "Estatus":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            empleados = empleados.OrderBy(x => x.Estatus).ToList();
+                            break;
+                        case "Desc":
+                            empleados = empleados.OrderByDescending(x => x.Estatus).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+                default:
+                    empleados = empleados.OrderBy(x => x.Nombre).ToList();
+                    break;
+            }
+
+            ViewBag.TotalPages = Math.Ceiling(empleados.Count() / 10.0);
+            int page = int.Parse(Page == null ? "1" : Page);
+            ViewBag.Page = page;
+
+            empleados = empleados.Skip((page - 1) * 10).Take(10);
 
 
-            return View();
+            return View(empleados);
         }
 
         [HttpGet]
