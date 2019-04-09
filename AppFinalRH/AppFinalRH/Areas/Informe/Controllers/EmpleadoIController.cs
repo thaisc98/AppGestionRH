38a3764ec83e46567322072ce2400c97647f39e7
Miguel319 <miguelp90.319@gmail.com>
2019-04-08 20:26:14 -0400

    Cambios revertidos

--- AppFinalRH/AppFinalRH/Areas/Informe/Controllers/EmpleadoIController.cs
+++ AppFinalRH/AppFinalRH/Areas/Informe/Controllers/EmpleadoIController.cs
@@ -13,7 +13,7 @@ namespace AppFinalRH.Areas.Informe.Controllers
         public EmpleadoIController() => empldn = new EmpleadoLDN();
 
         // GET: Informe/EmpleadoI
-        public ActionResult Index(string Estatus, string buscarPor, string buscar, string SortOrder, string SortBy,string Page)
+        public ActionResult Index(string Estatus, string buscarPor, string buscar, string Page)
         {
             if (buscarPor == "Departamento")
             {
@@ -36,118 +36,6 @@ namespace AppFinalRH.Areas.Informe.Controllers
             }
 
             var abc = empldn.GetActives();
-
-
-            ViewBag.SortOrder = SortOrder;
-            ViewBag.SortBy = SortBy;
-            var empleados = empldn.GetAll();
-
-            switch (SortBy)
-            {
-                case "CodigoEmp":
-                    switch (SortOrder)
-                    {
-                        case "Asc":
-                            empleados = empleados.OrderBy(x => x.CodigoEmp).ToList();
-                            break;
-                        case "Desc":
-                            empleados = empleados.OrderByDescending(x => x.CodigoEmp).ToList();
-                            break;
-                    }
-                    break;
-                case "Nombre":
-                    switch (SortOrder)
-                    {
-                        case "Asc":
-                            empleados = empleados.OrderBy(x => x.Nombre).ToList();
-                            break;
-                        case "Desc":
-                            empleados = empleados.OrderByDescending(x => x.Nombre).ToList();
-                            break;
-                    }
-                    break;
-                case "Edad":
-                    switch (SortOrder)
-                    {
-                        case "Asc":
-                            empleados = empleados.OrderBy(x => x.Edad).ToList();
-                            break;
-                        case "Desc":
-                            empleados = empleados.OrderByDescending(x => x.Edad).ToList();
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-               case "Telefono":
-                    switch (SortOrder)
-                    {
-                        case "Asc":
-                            empleados = empleados.OrderBy(x => x.Telefono).ToList();
-                            break;
-                        case "Desc":
-                            empleados = empleados.OrderByDescending(x => x.Telefono).ToList();
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case "FechaIngreso":
-                    switch (SortOrder)
-                    {
-                        case "Asc":
-                            empleados = empleados.OrderBy(x => x.FechaIngreso).ToList();
-                            break;
-                        case "Desc":
-                            empleados = empleados.OrderByDescending(x => x.FechaIngreso).ToList();
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case "Salario":
-                    switch (SortOrder)
-                    {
-                        case "Asc":
-                            empleados = empleados.OrderBy(x => x.FechaIngreso).ToList();
-                            break;
-                        case "Desc":
-                            empleados = empleados.OrderByDescending(x => x.FechaIngreso).ToList();
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case "Estatus":
-                    switch (SortOrder)
-                    {
-                        case "Asc":
-                            empleados = empleados.OrderBy(x => x.Estatus).ToList();
-                            break;
-                        case "Desc":
-                            empleados = empleados.OrderByDescending(x => x.Estatus).ToList();
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case "CodigoCargo":
-                    switch (SortOrder)
-                    {
-                        case "Asc":
-                            empleados = empleados.OrderBy(x => x.Cargo.CodigoCargo).ToList();
-                            break;
-                        case "Desc":
-                            empleados = empleados.OrderByDescending(x => x.Cargo.CodigoCargo).ToList();
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                default:
-                    empleados = empleados.OrderBy(x => x.CodigoEmp).ToList();
-                    break;
-            }
 
             ViewBag.TotalPages = Math.Ceiling(abc.Count() / 10.0);
             int page = int.Parse(Page == null ? "1" : Page);
